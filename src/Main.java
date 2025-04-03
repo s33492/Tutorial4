1b2d5e826f3f1db698ee8c1068f4c274720dc7b3
s33492 <s33492@pjwstk.edu.pl>
2025-04-03 19:14:37 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ReservationSystem reservationSystem = new ReservationSystem();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Event e1 = new Event("something",3.50);
+        Event e2 = new Event("Weezer",1000000,"21st february","Mcdonald's basement");
+        System.out.println(e1.getLocation());
+        System.out.println(e2.getLocation());
+        System.out.println();
+
+        reservationSystem.addEventToTotalList(e1);
+        reservationSystem.addEventToTotalList(e2);
+        System.out.println();
+
+        Customer customer1 = new Customer("Joe","Mama");
+        Customer customer2 = new Customer("John","Pork","JohnPork@gmail.com");
+        reservationSystem.addCustomerToTotalList(customer1);
+        reservationSystem.addCustomerToTotalList(customer2);
+        System.out.println();
+
+        reservationSystem.makeReservation(customer1,e1);
+        reservationSystem.makeReservation(customer2,e1);
+        reservationSystem.makeReservation(customer1,e2);
+        System.out.println();
+
+        System.out.println(customer1.getReservationList());
+        System.out.println();
+
+        reservationSystem.findEvent("Freakbob");
+        reservationSystem.findEvent("Weezer");
+        System.out.println();
+
+        reservationSystem.findCustomer("John","Pork");
+        reservationSystem.findCustomer("Hugh","Mungus");
+        reservationSystem.findCustomer("Freak","Bob");
+        System.out.println();
+
+        reservationSystem.changeEventPrice("something",30);
+        customer1.displayReservationList();
+        System.out.println();
+
+        customer2.removeFromReservationList(e2);
+        System.out.println("seats available: " + e2.getAvailableSeats());
+
+        System.out.println("seats available: " + e1.getAvailableSeats());
+        System.out.println();
+
     }
+
 }
